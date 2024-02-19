c9f6c3d62f97d38a1827580f748c5464dbe573ef
김동민 <101377860+keastmin@users.noreply.github.com>
2024-02-20 03:38:30 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.cpp
+++ 프로그래머스/1/12901. 2016년/2016년.cpp
@@ -5,7 +5,6 @@
 using namespace std;
 
 string solution(int a, int b) {
-    //vector<int> day = {31, 29, 31, 30, 31, 30, 31, 31, 30, 29, 30, 31};
     vector<string> week = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
     vector<int> start = {5, 1, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4};
     
