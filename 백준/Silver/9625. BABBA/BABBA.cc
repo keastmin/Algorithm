eaeaf30cfc8655a0073baf7bd3352d39e1949af4
김동민 <101377860+keastmin@users.noreply.github.com>
2024-05-26 14:48:38 +0900

    [Silver V] Title: BABBA, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9625. BABBA/BABBA.cc
+++ 백준/Silver/9625. BABBA/BABBA.cc
@@ -1,24 +1,9 @@
 #include<iostream>
-#include <vector>
 using namespace std;
-
 int main() {
-	int K;
+	int K, A = 1, B = 0, t;
 	cin >> K;
-
-	vector<pair<int, int>> dp(K + 1);
-	dp[0] = { 1, 0 };
-
-	for (int i = 1; i <= K; i++) {
-		int A = dp[i - 1].first;
-		int B = dp[i - 1].second;
-
-		dp[i] = { B, B + A };
-
-		//cout << i << ", " << dp[i].first << ", " << dp[i].second << '\n';
-	}
-
-	cout << dp[K].first << ' ' << dp[K].second << '\n';
-
+	while (K--) { t = A; A = B; B += t; }
+	cout << A << ' ' << B;
 	return 0;
 }
