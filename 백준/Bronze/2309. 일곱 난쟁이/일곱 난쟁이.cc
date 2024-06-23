498bf8eb24fd7275ea8787ef207ff51577fd7147
김동민 <101377860+keastmin@users.noreply.github.com>
2024-06-23 17:42:24 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -6,20 +6,19 @@ using namespace std;
 #define PEO 9
 
 vector<int> arr(9);
+int sum = 0;
 
 int main() {
-	for (int i = 0; i < 9; i++) cin >> arr[i];
+
+	for (int i = 0; i < 9; i++) {
+		cin >> arr[i];
+		sum += arr[i];
+	}
 	sort(arr.begin(), arr.end());
 
 	for (int i = 0; i < PEO - 1; i++) {
 		for (int j = i + 1; j < PEO; j++) {
-			int sum = 0;
-			for (int k = 0; k < PEO; k++) {
-				if (k != i && k != j) {
-					sum += arr[k];
-				}
-			}
-			if (sum == 100) {
+			if (sum - (arr[i] + arr[j]) == 100) {
 				for (int k = 0; k < PEO; k++) {
 					if (k != i && k != j) {
 						cout << arr[k] << '\n';
@@ -29,7 +28,6 @@ int main() {
 			}
 		}
 	}
-
 
 	return 0;
 }
