4213e94ec9d9b4d2e90b57d856d1509b9e456ffd
김동민 <101377860+keastmin@users.noreply.github.com>
2024-07-09 16:35:40 +0900

    [Gold II] Title: 빵집, Time: 92 ms, Memory: 8464 KB -BaekjoonHub

--- 백준/Gold/3109. 빵집/빵집.cc
+++ 백준/Gold/3109. 빵집/빵집.cc
@@ -10,7 +10,6 @@ int dx[3] = { 1, 1, 1 };
 int dy[3] = { -1, 0, 1 };
 
 int res = 0;
-bool isFind = false;
 
 void Input() {
 	cin >> R >> C;
@@ -26,10 +25,38 @@ void Input() {
 bool CheckOverGrid(int x, int y) {
 	if (y < 0 || y >= R) return false;
 	else if (map[y][x] == 'x') return false;
-	//else if (visited[y][x]) return false;
 	return true;
 }
 
+// 기존 로직
+/************************
+void BackTrack(int x, int y) {
+	if (isFind) return;
+
+	visited[y][x] = true;
+
+	if (x == C - 1) {
+		isFind = true;
+		res++;
+		return;
+	}
+
+	for (int i = 0; i < 3; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+		if (CheckOverGrid(nx, ny)) {
+			BackTrack(nx, ny);
+		}
+	}
+
+	if (!isFind) {
+		visited[y][x] = false;
+	}
+}
+*************************/
+
+// 기존의 방식은 일단 함수에 진입한 후 체크를 진행하기 때문에 함수 진입 오버헤드가 제법 큼
+// 하지만 아래와 같은 방식은 함수에 진입하기 전 true 상태를 인지하여 조건문에서 걸러짐
 bool BackTrack(int x, int y) {
 	if (x == C - 1) {
 		res++;
@@ -37,7 +64,6 @@ bool BackTrack(int x, int y) {
 	}
 
 	map[y][x] = 'x';
-
 
 	for (int i = 0; i < 3; i++) {
 		int nx = x + dx[i];
